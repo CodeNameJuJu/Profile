02572f42a60f6ca3e5a7b894334933e6311861aa
CodeNameJuJu <julianstone98@gmail.com>
2023-06-30 12:10:45 +0200

    Style changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -35,7 +35,7 @@ export const Contact = () => {
           <tr>
             <td><a href='mailto:julianstone98@gmail.com' title='Email Link'>julianstone98@gmail.com</a></td>
             <td><a href='https://www.linkedin.com/in/julian-stone-b80837215/' title='Linkedin URL'>Julian Stone</a></td>
-            <td><a href='https://github.com/Fishpuke' title='Github URL'>CodeNameJuJu</a></td>
+            <td><a href='https://github.com/CodeNameJuJu' title='Github URL'>CodeNameJuJu</a></td>
           </tr>
         </tbody>
       </table>
