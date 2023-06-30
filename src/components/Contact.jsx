9ae85033ca2b4b096b6209b7e8cbd11f1c2c6977
CodeNameJuJu <julianstone98@gmail.com>
2023-06-30 11:11:03 +0200

    Contact form functionality

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,27 +1,22 @@
-import React, { useState } from 'react';
+import React, {useRef} from 'react';
 import './Footer.css';
+import emailjs from '@emailjs/browser';
 
-const FORM_ENDPOINT = "https://public.herotofu.com/v1/f6888f80-ebdc-11ec-94cd-436cd72d679b"; // TODO - fill on the later step
+export const Contact = () => {
+    const form = useRef();
 
-const Contact = () => {
-  const [submitted, setSubmitted] = useState(false);
-  const handleSubmit = () => {
-    setTimeout(() => {
-      setSubmitted(false);
-    }, 100);
-  };
+    const sendEmail = (e) => {
+        e.preventDefault();
 
-  if (submitted) {
-    return (
-      <>
-        <div className="text-2xl">Thank you!</div>
-        <div className="text-md">We'll be in touch soon.</div>
-      </>
-    );
-  }
+        emailjs.sendForm('service_y9k7a7e', 'template_vuclici', form.current, 'PrZ7kEJQwuAOirfKf')
+            .then((result) => {
+                alert('message sent successfully...');
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            })
+    };
 
-  window.scrollTo(0, 0);
-  
   return (
   <body class='body-text center-again'>
     <div>
@@ -34,58 +29,36 @@ const Contact = () => {
             <th title='Email'><i class='bx bxs-envelope' ></i>Email</th>
             <th title='Linkedin'><i class='bx bxl-linkedin-square'></i>Linkedin</th>
             <th title='Github'><i class='bx bxl-github' ></i>Github</th>
-            <th title='Cellphone'><i class='bx bxs-phone'></i>Mobile</th>
           </tr>
         </thead>
         <tbody>
           <tr>
             <td><a href='mailto:julianstone98@gmail.com' title='Email Link'>julianstone98@gmail.com</a></td>
             <td><a href='https://www.linkedin.com/in/julian-stone-b80837215/' title='Linkedin URL'>Julian Stone</a></td>
-            <td><a href='https://github.com/Fishpuke' title='Github URL'>Fishpuke</a></td>
-            <td title='Cellphone'>073 132 1496</td>
+            <td><a href='https://github.com/Fishpuke' title='Github URL'>CodeNameJuJu</a></td>
           </tr>
         </tbody>
       </table>
     </div>
-    <form
-      action={FORM_ENDPOINT}
-      onSubmit={handleSubmit()}
-      method="POST"
-      target="_blank"
-    >
+    <form  ref={form} onSubmit={sendEmail}>
       <div className="mb-3 pt-0">
-        <input
-          type="text"
-          placeholder="Name"
-          name="name"
-          className="form-control px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
-          required
-        />
+          <input type="text" name="user_name" placeholder="Your Name"
+                  className="form-control px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+          />
       </div>
       <div className="mb-3 pt-0">
-        <input
-          type="email"
-          placeholder="Email"
-          name="email"
-          className="form-control px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
-          required
-        />
+          <input type="email" name="user_email" placeholder="Your email address"
+                  className="form-control px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+          />
       </div>
       <div className="mb-3 pt-0">
-        <textarea
-          placeholder="Message"
-          name="message"
-          className="form-control px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
-          required
-        />
+          <textarea name="message" placeholder="Your message"
+                    className="form-control px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+          ></textarea>
       </div>
+      <br />
       <div className="mb-3 pt-0">
-        <button
-          className="btn btn-primary"
-          type="submit"
-        >
-          Send message
-        </button>
+          <button className = "btn btn-primary" type="submit">Send Message</button>
       </div>
     </form>
   </body>
