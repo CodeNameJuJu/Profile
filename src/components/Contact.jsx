384afa6e932d37bbac4b08e84c88c76a6a678a96
CodeNameJuJu <julianstone98@gmail.com>
2023-09-19 14:33:39 +0200

    updated packages to fix env variables

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,10 @@ export const Contact = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm({process.env.REACT_APP_SERVICE_ID}, {process.env.REACT_APP_TEMPLATE_ID}, form.current, {process.env.REACT_APP_PUBLIC_KEY})
+    emailjs.sendForm({process.env.REACT_APP_SERVICE_ID}, 
+                     {process.env.REACT_APP_TEMPLATE_ID}, 
+                     form.current, 
+                     {process.env.REACT_APP_PUBLIC_KEY})
       .then((result) => {
         alert('message sent successfully...');
         console.log(result.text);
